d5d7cff6549b49d8078ebe16f1f35bbaecc5a91c
Sattvik <sattvikbhogade@gmail.com>
2026-08-10 19:24:18 +0530

    Time: 0 ms (100%), Space: 19.2 MB (90.19%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,23 +1,22 @@
 class Solution:
-    def rob(self, nums: List[int]) -> int:
-        n = len(nums)
-        dp = [-1] * n
-
-        def helper(i):
-            if i == 0:
-                return nums[0]
-
-            if i == 1:
-                return max(nums[0], nums[1])
-
-            if dp[i] != -1:
-                return dp[i]
-
-            skip = helper(i - 1)
-            take = nums[i] + helper(i - 2)
-
-            dp[i] = max(skip, take)
-
+    #Memoization - Top to Down
+    def helper(self, i, nums, dp):
+        #Base case...
+        if i == 0:
+            return nums[0]
+        if i == 1:
+            return max(nums[0], nums[1])
+        if dp[i] != -1:
             return dp[i]
 
-        return helper(n - 1)
+        pick = nums[i] + self.helper(i-2, nums, dp)
+        notPick = 0 + self.helper(i - 1, nums, dp)
+
+        dp[i] = max(pick, notPick)
+
+        return dp[i]
+
+    def rob(self, nums: List[int]) -> int:
+        dp = [-1] * len(nums)
+
+        return self.helper(len(nums) - 1, nums, dp)
