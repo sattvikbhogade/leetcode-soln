02401074a773c3409ce0ca1e01a3199d55d9562b
Sattvik <sattvikbhogade@gmail.com>
2026-08-10 19:28:04 +0530

    Time: 0 ms (100%), Space: 19.3 MB (23.56%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,22 +1,19 @@
 class Solution:
-    #Memoization - Top to Down
-    def helper(self, i, nums, dp):
-        #Base case...
-        if i == 0:
-            return nums[0]
-        if i == 1:
-            return max(nums[0], nums[1])
-        if dp[i] != -1:
-            return dp[i]
-
-        pick = nums[i] + self.helper(i-2, nums, dp)
-        notPick = 0 + self.helper(i - 1, nums, dp)
-
-        dp[i] = max(pick, notPick)
-
-        return dp[i]
+    #Tabulation - Bottom Up
+    
 
     def rob(self, nums: List[int]) -> int:
-        dp = [-1] * len(nums)
+        n = len(nums)
+        dp = [0] * n 
+        dp[0] = nums[0]
+        if n == 1:
+            return nums[0]
+        dp[1] = max(nums[0], nums[1])
 
-        return self.helper(len(nums) - 1, nums, dp)
+        for i in range(2, n):
+            pick = nums[i] + dp[i - 2]
+            not_pick = dp[i - 1]
+            dp[i] = max(pick, not_pick)
+        return dp[n - 1]
+#T.C.= O(n)
+#S.C.= O(n)
